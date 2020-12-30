62290a8fcc9a9686fba00ac4e53ae3aa4fb4ba68
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-12-31 00:12:34 +0100

    Serve Html file woth node

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
 var http = require('http');
-var module = require('./module');
+var fs = require('fs');
+var url = require('url');
+
+const page404 = fs.readFileSync("404.html", "utf-8", (err, data) => {
+  if (err) throw err;
+  return data;
+});
 
 http.createServer((req,res)=>{
-  res.writeHead(200, {"Content-Type": "text/plain"});
-  res.end("Hello world "+ module.myDateTime())
+let q = url.parse(req.url, true);
+let filename = "" 
+  if(q.pathname == '/'){
+    filename = "."+ "/index.html"
+  }else {
+    filename = "." + q.pathname
+  }
 
-}).listen(8081)
+  fs.readFile(filename, function(err, data){
+    if(err){
+      res.writeHead(404, {"Content-Type": "text/html"})
+      res.write(page404)
+      return res.end();
+    }
+
+    res.writeHead(200, {"Content-Type": "text/html"})
+    res.write(data)
+    return res.end();
+
+  })
+ 
+ 
+ 
+}).listen(8080)
