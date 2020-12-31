0821a75329d321cf3244eff20c8603529c0ad9b6
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-12-31 12:01:26 +0100

    Add express to project

--- server.js
+++ server.js
@@ -1,34 +1,27 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+ var http =require('http')
+ var url = require('url')
+ var fs = require('fs')
 
-const page404 = fs.readFileSync("404.html", "utf-8", (err, data) => {
-  if (err) throw err;
-  return data;
-});
 
-http.createServer((req,res)=>{
-let q = url.parse(req.url, true);
-let filename = "" 
-  if(q.pathname == '/'){
-    filename = "."+ "/index.html"
-  }else {
-    filename = "." + q.pathname
+ http.createServer((req,res)=>{
+  let q = url.parse(req.url, true)
+  let filename = ""
+
+  if(q.pathname === '/' ){
+    filename= "." + "/index.html"
+  }else{
+    filename = q.pathname
   }
 
-  fs.readFile(filename, function(err, data){
-    if(err){
-      res.writeHead(404, {"Content-Type": "text/html"})
-      res.write(page404)
-      return res.end();
-    }
-
-    res.writeHead(200, {"Content-Type": "text/html"})
-    res.write(data)
-    return res.end();
+  fs.readFile(filename, (err, data)=>{
+    if(err) { 
+    res.writeHead(404,{ "Content-Type":"text/html"})
+    return res.end()}
+    res.writeHead(200, {"Content-Type":"text/html"})
+      res.write(data)
+    return res.end()
 
   })
- 
- 
- 
-}).listen(8080)
+
+
+ }).listen(3000)
