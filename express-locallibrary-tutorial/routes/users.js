462ba42ad48d151c2ebd6598c8c0e39505308ea3
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-01-01 12:56:00 +0100

    Add a new route using next()

--- express-locallibrary-tutorial/routes/users.js
+++ express-locallibrary-tutorial/routes/users.js
@@ -4,6 +4,11 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+  next()
+});
+
+router.get('/cool', function(req, res, next) {
+  res.send('You are so cool!');
 });
 
 module.exports = router;
