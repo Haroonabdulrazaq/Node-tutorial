52d8be7282eed258148985f06bf99fbb748c0be7
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-01-01 23:57:35 +0100

    Seed data into the database

--- express-locallibrary-tutorial/models/author.js
+++ express-locallibrary-tutorial/models/author.js
@@ -1,31 +1,36 @@
 var mongoose = require('mongoose');
 
-var Schema = mongoose.Schema
+var Schema = mongoose.Schema;
 
-var AuthorSchema = new Schema({
-  first_name: {type: String, required: true, maxlength:100},
-  family_name: {type: String, required: true, maxlength:100},
-  date_of_birth: {type: Date},
-  date_of_death: {type: Date}
-})
+var AuthorSchema = new Schema(
+  {
+    first_name: {type: String, required: true, maxlength: 100},
+    family_name: {type: String, required: true, maxlength: 100},
+    date_of_birth: {type: Date},
+    date_of_death: {type: Date},
+  }
+);
 
-// Derived Data for name
+// Virtual for author's full name
 AuthorSchema
 .virtual('name')
-.get(function(){
-  return this.family_name +", "+ this.first_name
+.get(function () {
+  return this.family_name + ', ' + this.first_name;
 });
 
+// Virtual for author's lifespan
 AuthorSchema
 .virtual('lifespan')
-.get(function(){
+.get(function () {
   return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
-})
+});
 
+// Virtual for author's URL
 AuthorSchema
 .virtual('url')
-.get(function(){
-  return '/catalog/author'+ this._id
-})
+.get(function () {
+  return '/catalog/author/' + this._id;
+});
 
-module.exports = mongoose.model("Author", AuthorSchema)
+//Export model
+module.exports = mongoose.model('Author', AuthorSchema);
--- express-locallibrary-tutorial/models/book.js
+++ express-locallibrary-tutorial/models/book.js
@@ -1,19 +1,23 @@
 var mongoose = require('mongoose');
 
-var Schema = mongoose.Schema 
+var Schema = mongoose.Schema;
 
-var BookSchema = new Schema({
-  title: {type: String, required: true},
-  author:{type: Schema.Types.ObjectId, ref: "Author" , required:true},
-  summary:{type: String, required: true},
-  isbn: {type: String, required: true},
-  genre: {type: Schema.Types.ObjectId, ref: "Genere"}
-})
+var BookSchema = new Schema(
+  {
+    title: {type: String, required: true},
+    author: {type: Schema.Types.ObjectId, ref: 'Author', required: true},
+    summary: {type: String, required: true},
+    isbn: {type: String, required: true},
+    genre: [{type: Schema.Types.ObjectId, ref: 'Genre'}]
+  }
+);
 
+// Virtual for book's URL
 BookSchema
 .virtual('url')
-.get(function(){
-  return '/catalog/book'+ this._id
-})
+.get(function () {
+  return '/catalog/book/' + this._id;
+});
 
-module.exports = mongoose.model("Book", BookSchema)
+//Export model
+module.exports = mongoose.model('Book', BookSchema);
