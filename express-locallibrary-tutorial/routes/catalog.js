9859f1dbbea1250572feb98d8520423964c01b16
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-01-02 15:25:07 +0100

    Add bookinstance route and callback

--- express-locallibrary-tutorial/routes/catalog.js
+++ express-locallibrary-tutorial/routes/catalog.js
@@ -80,8 +80,31 @@ router.get('/genre/:id', genre_controller.genre_detail)
 router.get('/genres', genre_controller.genre_list)
 
 
+// ====================================BookInstance=========================================================
 
+// GET request for creating a BookInstance. NOTE This must come before route that displays BookInstance (uses id).
+router.get('/bookinstance/create', book_instance_controller.bookinstance_create_get);
 
+// POST request for creating BookInstance.
+router.post('/bookinstance/create', book_instance_controller.bookinstance_create_post);
+
+// GET request to delete BookInstance.
+router.get('/bookinstance/:id/delete', book_instance_controller.bookinstance_delete_get);
+
+// POST request to delete BookInstance.
+router.post('/bookinstance/:id/delete', book_instance_controller.bookinstance_delete_post);
+
+// GET request to update BookInstance.
+router.get('/bookinstance/:id/update', book_instance_controller.bookinstance_update_get);
+
+// POST request to update BookInstance.
+router.post('/bookinstance/:id/update', book_instance_controller.bookinstance_update_post);
+
+// GET request for one BookInstance.
+router.get('/bookinstance/:id', book_instance_controller.bookinstance_detail);
+
+// GET request for list of all BookInstance.
+router.get('/bookinstances', book_instance_controller.bookinstance_list);
 
 
 
