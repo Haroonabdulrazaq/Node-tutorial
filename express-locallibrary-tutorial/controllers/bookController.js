63e78f3a1ce8b84b751c966bfc95c0801c34cf2d
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-01-02 22:45:03 +0100

    Remove placeholder from index controller

--- express-locallibrary-tutorial/controllers/bookController.js
+++ express-locallibrary-tutorial/controllers/bookController.js
@@ -1,7 +1,30 @@
 var Book = require('../models/book');
+var Author = require('../models/author');
+var Genre = require('../models/genre');
+var BookInstance = require('../models/bookinstance');
+
 
 exports.index = function(req, res) {
-    res.send('NOT IMPLEMENTED: Site Home Page');
+    // res.send('NOT IMPLEMENTED: Site Home Page');
+    asyn.parallel({
+        book_count: function(callback){
+            Book.countDocuments({}, callback)
+        },
+        book_instance_count: function(callback){
+            BookInstance.countDocuments({}, callback)
+        },
+        book_instance_available_count: function(callback){
+            BookInstance.countDocuments({status:'Available', callback})
+        },
+        author_count: function(callback){
+            Author.countDocuments({}, callback)
+        },
+        genre_count: function(callback){
+            Genre.countDocuments({}, callback)
+        }
+    }, function(err, results){
+        res.render('index', {title:"Local Library Home", error: err, data: results});
+    });
 };
 
 // Display list of all books.
