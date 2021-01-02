3f93681ae956a3592f83acbf0c8bff27fed5736a
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-01-02 11:29:01 +0100

    Add a new project messge-voard

---
+++ message-board/routes/index.js
@@ -0,0 +1,27 @@
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express', messages: messages });
+  next()
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('form');
+});
+
+module.exports = router;
