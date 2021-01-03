1b16f5c9d54f56cd8ed732f678e0ad44538064c4
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-01-03 13:40:39 +0100

    Add bookinstance list

--- express-locallibrary-tutorial/controllers/bookinstanceController.js
+++ express-locallibrary-tutorial/controllers/bookinstanceController.js
@@ -1,8 +1,14 @@
 var BookInstance = require('../models/bookinstance');
 
 // Display list of all BookInstances.
-exports.bookinstance_list = function(req, res) {
-  res.send('NOT IMPLEMENTED: BookInstance list');
+exports.bookinstance_list = function(req, res, next) {
+  // res.send('NOT IMPLEMENTED: BookInstance list');
+  BookInstance.find()
+  .populate('Book')
+  .exec(function(err, list_bookinstances){ 
+    if(err){return next(err)};
+    res.render('bookinstance_list', {title: "Book Instance List", bookinstance_list: list_bookinstances})
+  })
 };
 
 // Display detail page for a specific BookInstance.
