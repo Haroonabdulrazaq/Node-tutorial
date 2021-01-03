d2390fea2a1ddb250cf23b1ac25f5c780093706c
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-01-03 17:46:29 +0100

    start genre detail

--- express-locallibrary-tutorial/controllers/genreController.js
+++ express-locallibrary-tutorial/controllers/genreController.js
@@ -1,4 +1,6 @@
 var Genre = require('../models/genre');
+var Book = require('../models/book');
+var async = require('async');
 
 // Display list of all Genre.
 exports.genre_list = function(req, res) {
@@ -11,12 +13,27 @@ exports.genre_list = function(req, res) {
 };
 
 // Display detail page for a specific Genre.
-// exports.genre_detail = function(req, res) {
-//     res.send('NOT IMPLEMENTED: Genre detail: ' + req.params.id);
-// };
-exports.genre_detail = function(req, res){
-    res.send('NOT IMPLEMENTED: Genre detail: ' + req.params.id)
-  } 
+exports.genre_detail = function(req, res, next) {
+    // res.send('NOT IMPLEMENTED: Genre detail');
+    async.parallel({
+        genre: function(callback){
+            Genre.findById(req.params.id)
+            .exec(callback)
+        },
+        genre_books: function(callback){
+            Book.find({ 'genre': req.params.id })
+            .exec(callback)
+        }
+    }, function(err, results){
+        if(err){return next(err)}
+        if(results.genre == null){
+            var err = new Error
+            err.status = 404
+            return next(err) 
+        }
+        res.render('genre_detail',{title: "Genre Details", genre: results.genre, genre_books: results.genre_books})
+    })
+};
 
 // Display Genre create form on GET.
 exports.genre_create_get = function(req, res) {
