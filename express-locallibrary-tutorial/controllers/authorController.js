f84e251f21e288f6f17d9a046fdfa3e2ecf9acb0
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-01-03 14:20:26 +0100

    Add author list to project

--- express-locallibrary-tutorial/controllers/authorController.js
+++ express-locallibrary-tutorial/controllers/authorController.js
@@ -1,8 +1,13 @@
 var Author = require('../models/author')
 
 // Display list of all Authors.
-exports.author_list = function(req, res){
-  res.send('NOT IMPLEMENTED: Author list')
+exports.author_list = function(req, res, next){
+  // res.send('NOT IMPLEMENTED: Author list')
+  Author.find()
+  .exec(function(err, author_list){
+    if(err){return next(err)};
+    res.render("author_list", {title: "All Author", author_list: author_list })
+  })
 } 
 
 // Display detail page for a specific Author.
