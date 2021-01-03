139094b7df312f4da821ee7fcf3e30f43b23877c
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-01-03 13:19:14 +0100

    Add redirect to home page to /catlog

--- express-locallibrary-tutorial/routes/index.js
+++ express-locallibrary-tutorial/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('/catalog');
+  res.redirect('/catalog');
 });
 
 module.exports = router;
