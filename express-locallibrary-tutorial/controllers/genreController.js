0b0cef51bb2ae57d8857f84901a7772b29e2fe3a
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-01-03 15:45:38 +0100

    Add genere to project

--- express-locallibrary-tutorial/controllers/genreController.js
+++ express-locallibrary-tutorial/controllers/genreController.js
@@ -2,7 +2,12 @@ var Genre = require('../models/genre');
 
 // Display list of all Genre.
 exports.genre_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre list');
+    // res.send('NOT IMPLEMENTED: Genre list');
+    Genre.find()
+    .exec(function(err, genre){
+        if(err){return next(err)}
+        res.render('genre_list', {title: "Genre List", genre_name: genre})
+    })
 };
 
 // Display detail page for a specific Genre.
