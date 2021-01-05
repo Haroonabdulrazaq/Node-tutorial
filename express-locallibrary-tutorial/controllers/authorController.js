4f2d870646d7c40ee0a603bc43f8b580940543b9
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-01-05 15:28:58 +0100

    Add controller for author

--- express-locallibrary-tutorial/controllers/authorController.js
+++ express-locallibrary-tutorial/controllers/authorController.js
@@ -1,6 +1,7 @@
 var Author = require('../models/author')
 var Book = require('../models/book')
-var async =require('async')
+var async =require('async');
+var {body, validationResult} = require('express-validator')
 
 // Display list of all Authors.
 exports.author_list = function(req, res, next){
@@ -45,7 +46,7 @@ exports.author_create_get = function(req , res){
 }
 
 // Handle Author create on POST.
-exports.author_create_post = function(req,res){
+exports.author_create_post =  [
   // res.send('NOT IMPLEMENTED: Author create POST')
   // Sanitize and Validate input =>First Element
   // create anonymous function  =>Second Element
@@ -55,7 +56,50 @@ exports.author_create_post = function(req,res){
   // Query Database to see if it already Exist
   // Else save the new name
   // Redirect to url
-}
+  body('first_name')
+  .trim().isLength({min:2}).escape()
+  .withMessage('First name can not be blank.')
+  .isAlphanumeric().withMessage('First name has non-alphanumeric characters.'),
+
+  body('family_name')
+  .trim().isLength({min:2}).escape()
+  .withMessage('Family name can not be blank')
+  .isAlphanumeric().withMessage('Family name has non-alphanumeric characters.'),
+
+  body('date_of_birth', 'Invalid date of birth')
+  .optional({ checkFalsy: true }).isISO8601().toDate(),
+
+  body('date_of_death', 'Invalid date of death')
+  .optional({ checkFalsy: true }).isISO8601().toDate(),
+ 
+  (req,res, next)=>{
+    var errors = validationResult(req);
+    var author = new Author({
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body.date_of_death
+    })
+
+    if(!errors.isEmpty()){
+      res.render('author_form', {title: "Create Author", author: author, errors: errors.array()});
+      return;
+    }else{
+      Author.findOne({'name': req.body.first_name})
+      .exec(function(err, found_author){
+        if(err){return next(err)}
+        if(found_author){
+          res.redirect(found_author.url)
+        }else{
+          author.save(function(err){
+            if(err){return next(err)}
+            res.redirect(author.url)
+          })
+        }
+      })
+    }
+  }
+]
 
 // Display Author delete form on GET.
 exports.author_delete_get = function(req,res){
