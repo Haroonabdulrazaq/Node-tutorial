f41a55f994567b9814146bcc738d98a9bceefd25
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-01-13 12:48:03 +0100

    finished update get route

--- express-locallibrary-tutorial/controllers/bookController.js
+++ express-locallibrary-tutorial/controllers/bookController.js
@@ -216,8 +216,40 @@ exports.book_delete_post = function(req, res) {
 };
 
 // Display book update form on GET.
-exports.book_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book update GET');
+exports.book_update_get = function(req, res, next) {
+    // res.send('NOT IMPLEMENTED: Book update GET');
+    async.parallel({
+        book: function(callback){
+            Book.findById(req.params.id)
+            .populate(['author', 'genre'])
+            .exec(callback)
+        },
+        authors: function(callback){
+            Author.find(callback)
+        },
+
+        genres: function(callback){
+            Genre.find(callback)
+        }
+    }, function(err, results){
+        if(err){return next(err)}
+
+        if(results.book ===null){
+            var error = new Error("Book not found");
+            error.status = 404
+            return next(err)
+        }
+
+        for (var all_g_iter = 0; all_g_iter < results.genres.length; all_g_iter++) {
+            for (var book_g_iter = 0; book_g_iter < results.book.genre.length; book_g_iter++) {
+                if (results.genres[all_g_iter]._id.toString()===results.book.genre[book_g_iter]._id.toString()) {
+                    results.genres[all_g_iter].checked='true';
+                }
+            }
+        }
+
+        res.render('book_form', { title: 'Update Book', authors: results.authors, genres: results.genres, book: results.book });
+    })
 };
 
 // Handle book update on POST.
