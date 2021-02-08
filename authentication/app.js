29d213c04195053f4d71ae38646af71aebf66523
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-02-08 18:10:44 +0100

    Add View an index toviews

--- authentication/app.js
+++ authentication/app.js
@@ -1,7 +1,7 @@
 
 const express= require('express');
 const path = require('path');
-const session = requre('express-session');
+const session = require('express-session');
 const passport =require('passport');
 const LocalStrategy =require('passport-local').Strategy;
 const mongoose = require('mongoose');
@@ -9,7 +9,7 @@ const Schema = mongoose.Schema;
 
 const mongoDb = 'mongodb+srv://Haroonabdulrazaq:haroon123@cluster0.4o3qm.mongodb.net/AuthDB?retryWrites=true&w=majority';
 mongoose.connect(mongoDb, {useUnifiedTopology: true, useNewUrlParser: true});
-const db = mongoose.connection();
+const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "mongo connection error"));
 
@@ -20,3 +20,18 @@ const User = mongoose.model(
     password: {type: String, required: true}
   })
 )
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(express.urlencoded({extended: false}))
+
+app.get("/", (req, res) => res.render("index"));
+
+app.listen(3000, ()=> console.log("App listening on part 3000!!"))
